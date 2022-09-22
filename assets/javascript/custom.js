40e9c897b13590a934c4cd2506ab059183a41f8b
Ashcode <akshaycodeur@gmail.com>
2022-09-22 17:03:33 -0400

    Contact form initiated

--- assets/javascript/custom.js
+++ assets/javascript/custom.js
@@ -22,3 +22,41 @@ var typed = new Typed('.type', {
     });
     
 
+    $(document).ready(function(){
+        $('.projectslide').slick({
+        dots: true,
+        infinite: true,
+        speed: 900,
+        slidesToShow: 1,
+        fade:true,
+        slidesToScroll: 1,
+        responsive: [
+            {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+            },
+            {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+        });
+    });
