8fd799c30cb0e936a8e23e05dec0cc34c5411b4b
Ashcode <akshaycodeur@gmail.com>
2022-09-22 15:38:43 -0400

    Slick initiated

---
+++ assets/javascript/custom.js
@@ -0,0 +1,24 @@
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true,
+    easing: 'easeInOutCubic',
+    easingcss3: 'ease',
+    navigation: true,
+    navigationPosition:'right',
+    scrollingSpeed: 500
+});
+
+
+var typed = new Typed('.type', {
+    strings: ['Front End Developer', 'Remote Developer', 'UI/UX Designer'],
+    loop: true,
+    backSpeed: 100,
+    showCursor: true,
+    cursorChar: '|',
+    autoInsertCss: true,
+    typeSpeed: 100,
+    smartBackspace: true // Default value
+    });
+    
+
